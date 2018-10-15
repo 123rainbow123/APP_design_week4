67b5e9d8e3bd65a4201fee019e15dbcadfa0492d
123rainbow123 <1306349650@qq.com>
2018-10-15 20:43:54 +0800

    try
    try

---
+++ files/page1/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,Z,ba,bb,q,bc,bd,bc,be,bf,v,_(w,bg,bh,_(bi,bj,bk,bl)),S,_(),bm,_(),bn,_(bo,bp))])),bq,_(),br,_(bs,_(bt,bu)));}; 
+var b="url",c="page1.html",d="generationDate",e=new Date(1539599708382.28),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="640483340d55412187a07b3d2ab621b6",q="type",r="Axure:Page",s="name",t="page1",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="87eeec4f78c643c7bae0028e65d72413",Y="label",Z="手机注册",ba="friendlyType",bb="图片",bc="imageBox",bd="styleType",be="visible",bf=true,bg="75a91ee5b9d042cfa01b8d565fe289c0",bh="size",bi="width",bj=411,bk="height",bl=216,bm="imageOverrides",bn="images",bo="normal~",bp="images/page1/手机注册_u72.png",bq="masters",br="objectPaths",bs="87eeec4f78c643c7bae0028e65d72413",bt="scriptId",bu="u72";
+return _creator();
+})());
